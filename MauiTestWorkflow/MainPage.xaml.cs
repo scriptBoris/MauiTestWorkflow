68774e5c94185c28e937927927a7e7bfc7c8be7c
Boris Krit <scriptboris@gmail.com>
2024-02-19 06:03:19 +0900

    fix main page

--- MauiTestWorkflow/MainPage.xaml.cs
+++ MauiTestWorkflow/MainPage.xaml.cs
@@ -13,6 +13,22 @@
         {
             count++;
 
+#if DEVELOP
+            DisplayAlert("MSG", "DEVELOP mode", "OK");
+#endif
+
+#if DEBUG
+            DisplayAlert("MSG", "DEBUG mode", "OK");
+#endif
+
+#if ANDROID
+            DisplayAlert("MSG", "ANDROID mode", "OK");
+#endif
+
+#if NET8_0_OR_GREATER
+            DisplayAlert("MSG", "NET8> mode", "OK");
+#endif
+
             if (count == 1)
                 CounterBtn.Text = $"Clicked {count} time";
             else
