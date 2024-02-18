a7cb155f3493f364ef08c41e1dfce9122bbdca1b
Boris Krit <scriptboris@gmail.com>
2024-02-18 19:49:33 +0900

    Добавьте файлы проекта.

---
+++ MauiTestWorkflow/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MauiTestWorkflow
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
